edb405fe06f03566ba00b6570355264c65c18876
Rojimme <merodriguezji@est.utn.ac.cr>
2024-04-10 17:56:16 -0600

    agregado de zodiacos

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-		import {View} from 'react-native';
-		import Index from './src/views';
+import {View} from 'react-native';
+import Index from './src/views/index';
 
-		const App = () => {
-		  return(
-			  <View>
-				<Index />
-			  </View>
-		  );
-		};
-
-		export default App;
+const App = () => {
+  return (
+    <View>
+      <Index />
+    </View>
+  );
+};
+export default App;
--- src/views/index.js
+++ src/views/index.js
@@ -1,78 +1,156 @@
 import React from 'react';
-		import { Image, ScrollView,  Text, View} from 'react-native';
-		import {style_01} from '../styles/style_01';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { style_01 } from '../styles/style_01';
 
-		const data = [
-			{codigo : 'ITI-121',
-				nombre : 'Fundamentos de Programación',
-				nivel  : 1,
-				creditos : 6},
-			{codigo : 'ITI-122',
-				nombre : 'Comunicación Oral y Escrita',
-				nivel  : 1,
-				creditos : 2},
-			{codigo : 'ITI-123',
-				nombre : 'Fundamentos de Bases de Datos',
-				nivel  : 1,
-				creditos : 3},
-			{codigo : 'ITI-124',
-				nombre : 'Matemáticas para Computación I',
-				nivel  : 1,
-				creditos : 6},
-			{codigo : 'ID-TEC-1',
-				nombre : 'Inglés 1',
-				nivel  : 1,
-				creditos : 0},
-			{codigo : 'IIT-221',
-				nombre : 'Programación I',
-				nivel  : 2,
-				creditos : 6},
-			{codigo : 'IIT-222',
-				nombre : 'Plataformas Tecnológicas I',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'IIT-223',
-				nombre : 'Administración de Bases de Datos',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'IIT-224',
-				nombre : 'Matemáticas para Computación II',
-				nivel  : 2,
-				creditos : 3},
-			{codigo : 'ID-TEC-2',
-				nombre : 'Inglés 1',
-				nivel  : 2,
-				creditos : 0}
-				
-		];
 
-		const Index = () => {
-			return(
-				<View>
-					<View style={style_01.divHeader}>
-						<Image source={require('../imgs/logos/logo_universidad.png')} />
-					</View>
+const images = {
+  Acuario: require('../imgs/Zodiacal/03_Aries.png'),
+  Piscis: require('../imgs/Zodiacal/03_Aries.png'),
+  Aries: require('../imgs/Zodiacal/03_Aries.png'),
+  Tauro: require('../imgs/Zodiacal/03_Aries.png'),
+  Geminis: require('../imgs/Zodiacal/03_Aries.png'),
+  Cancer: require('../imgs/Zodiacal/03_Aries.png'),
+  Leo: require('../imgs/Zodiacal/03_Aries.png'),
+  Virgo: require('../imgs/Zodiacal/03_Aries.png'),
+  Libra: require('../imgs/Zodiacal/03_Aries.png'),
+  Escorpio: require('../imgs/Zodiacal/03_Aries.png'),
+  Sagitario: require('../imgs/Zodiacal/03_Aries.png'),
+  Capricornio: require('../imgs/Zodiacal/03_Aries.png'),
+};
 
-					<View style={style_01.divMain}>
-						<Text style={style_01.h1}>Demostración 01</Text>
-						<Text style={style_01.p1}>Desde el 2019, se está realizando el proceso de rediseño de la carrera, el cual implica la actualización de los contenidos en las asignaturas, así como las formas de mediación mas actuales, todo esto tomando en cuenta tambien diversas recomendaciones derivadas del proceso de autoevalación y acreditación de la carrera.</Text>
+const data = [
 
-						<ScrollView style={{marginTop:8}}>
-							{data.map(item => <View style={style_01.tarjeta} key={item.codigo} >
-								<Text>Código: {item.codigo}</Text>
-								<Text>Nombre: {item.nombre}</Text>
-								<Text>Nivel: {item.nivel}</Text>
-								<Text>Créditos: {item.creditos}</Text>
-							</View>)}
-						</ScrollView>
-					</View>
+  {
+    Signo: 'Acuario',
+    Dias: 'Ene/20 - Feb/18',
+    Elemento: "Aire",
+    Astro: "Urano",
+    Piedra: "Turquoise",
+    Imagen: images.Libra
+  },
+  {
+    Signo: 'Piscis',
+    Dias: 'Feb/19 - Mar/20',
+    Elemento: "Agua",
+    Astro: "Neptuno",
+    Piedra: "Ametyst",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Aries',
+    Dias: 'Mar/21 - Abr/19',
+    Elemento: "Fuego",
+    Astro: "Marte",
+    Imagen: images.Libra
+  },
+  {
+    Signo: 'Tauro',
+    Dias: 'Abr/20 - May/20',
+    Elemento: "Tierra",
+    Astro: "Venus",
+    Piedra: "Quarz",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Geminis',
+    Dias: 'May/21 - Jun/20',
+    Elemento: "Aire",
+    Astro: "Mercurio",
+    Piedra: "Agate",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Cancer',
+    Dias: 'Jun/21 - Jul/22',
+    Elemento: "Agua",
+    Astro: "Luna",
+    Piedra: "Aventurine",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Leo',
+    Dias: 'Jul/23 - Ago/22',
+    Elemento: "Fuego",
+    Astro: "Sol",
+    Piedra: "Honey Agate",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Virgo',
+    Dias: 'Ago/23 - Sep/22',
+    Elemento: "Tierra",
+    Astro: "Mercurio",
+    Piedra: "Carnelian",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Libra',
+    Dias: 'Sep/23 - Oct/22',
+    Elemento: "Aire",
+    Astro: "Venus",
+    Piedra: "Jasper",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Escorpio',
+    Dias: 'Oct/23 - Nov/21',
+    Elemento: "Agua",
+    Astro: "Urano",
+    Piedra: "Chrysoprase",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Sagitario',
+    Dias: 'Nov/22 - Dic/21',
+    Elemento: "Fuego",
+    Astro: "Jupiter",
+    Piedra: "Blue Agate",
+	Imagen: images.Libra
+  },
+  {
+    Signo: 'Capricornio',
+    Dias: 'Dic/22 - Ene/19',
+    Elemento: "Tierra",
+    Astro: "Saturno",
+    Piedra: "Blue Quartz",
+	Imagen: images.Libra
+  },
+];
 
-					<View style={style_01.divFooter}>
-						<Text style={style_01.textFooter}>Carrera de Tecnologías de Información</Text>
-						<Text style={style_01.textFooter}>Sede del Pacífico</Text>
-					</View>
-				</View>
-			);
-		}
+const Index = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={style_01.divHeader}>
+        <Image source={require('../imgs/logos/logo_universidad.png')} style={{ width: 100, height: 50 }} />
+      </View>
 
-		export default Index;
+      <View style={style_01.divMain}>
+        <Text style={style_01.h1}>Signos Zodiacales</Text>
+        <Text style={style_01.p1}>Práctica de añadir los signos zodiacales</Text>
+
+        <ScrollView style={{ marginTop: 8 }}>
+          {data.map((item, index) => (
+            <View style={style_01.tarjeta} key={index}>
+              <Text>Signo: {item.Signo}</Text>
+              <Text>Días: {item.Dias}</Text>
+              <Text>Elemento: {item.Elemento}</Text>
+              <Text>Astro: {item.Astro}</Text>
+              <Text>Piedra: {item.Piedra}</Text>
+              <Image source={item.Imagen} style={{ width: 80, height: 80, left: 200, top: -75}} />
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+
+      <View style={style_01.divFooter}>
+        <Text style={style_01.textFooter}>
+          Carrera de Tecnologías de Información
+        </Text>
+        <Text style={style_01.textFooter}>Sede del Pacífico</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Index;
+
